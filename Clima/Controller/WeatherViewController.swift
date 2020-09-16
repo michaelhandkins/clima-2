80c1c15cdd01ccbd2be7a74914a51ee32bb9ff08
Michael Handkins <michaelhandkins@gmail.com>
2020-09-16 17:32:05 -0500

    Add command for locationManager to stop updating location once it has found the user's location after the location request is made

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -99,6 +99,7 @@ extension WeatherViewController: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.last {
+            locationManager.stopUpdatingLocation()
             let lat = location.coordinate.latitude
             let lon = location.coordinate.longitude
             weatherManager.fetchWeather(latitude: lat, longitude: lon)
