693fcdaf5af2eac2806e5e75e44fe7e20d87b28a
Michael Handkins <michaelhandkins@gmail.com>
2020-09-16 13:40:21 -0500

    Create networking function in WeatherManager

--- Clima/WeatherManager.swift
+++ Clima/WeatherManager.swift
@@ -10,10 +10,32 @@ import Foundation
 
 struct WeatherManager {
     
-    let weatherURL = "http://api.openweathermap.org/data/2.5/weather?q="
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?q="
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)" + "\(cityName)" + "&units=imperial&appid=8c5a6f258e6582f6de7e2ba455fd2800"
+        performRequest(urlString)
+    }
+    
+    func performRequest(_ urlString: String) {
+        
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    let dataString = String(data: safeData, encoding: .utf8)
+                    print(dataString!)
+                }
+            }
+            
+            task.resume()
+        }
+        
     }
     
 }
