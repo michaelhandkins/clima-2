e05a682acedfacbaeb17b63ca1de857bee8a4903
Michael Handkins <michaelhandkins@gmail.com>
2020-09-19 12:25:25 -0500

    Change name of app and add gifs for readme

--- What's The Weather?/WeatherData.swift
+++ What's The Weather?/WeatherData.swift
@@ -1,9 +1,7 @@
 //
 //  WeatherData.swift
-//  Clima
 //
 //  Created by Michael Handkins on 9/16/20.
-//  Copyright © 2020 App Brewery. All rights reserved.
 //
 
 import Foundation
--- What's The Weather?/WeatherManager.swift
+++ What's The Weather?/WeatherManager.swift
@@ -1,9 +1,7 @@
 //
 //  WeatherManager.swift
-//  Clima
 //
 //  Created by Michael Handkins on 9/16/20.
-//  Copyright © 2020 App Brewery. All rights reserved.
 //
 
 import Foundation
